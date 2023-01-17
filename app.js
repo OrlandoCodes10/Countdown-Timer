b37987093961cffd2b707cd32af4dca245857a84
OrlandoCodes10 <orlandofranco.novesteras@gmail.com>
2023-01-17 03:02:01 -0500

    Figuring out the time in seconds

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
-const newYear = '1 Jan 2024';
+const newYears = '1 Jan 2024';
 
 function countdown() {
     const newYearsDate = new Date(newYears);
     const currentDate = new Date();
 
-    console.log(newYearsDate - currentDate);
+    const Totalseconds = (newYearsDate - currentDate) / 1000;
+
+    const days = Math.floor(Totalseconds / 3600 / 24);
+    const hours = Math.floor(Totalseconds / 3600) % 24;
+    const minutes = Math.floor(Totalseconds / 60) % 60;
+    const seconds = Math.floor(Totalseconds)
+
+    console.log(days, hours);
 }
 
 // initial call
